acfb521d8427ddadda8f13dab64ba3881b8d2f59
vinh.thien0301 <vinh.thien0301@gmail.com>
2014-11-17 11:59:33 +0700

    init

---
+++ PrayerLine/platforms/android/src/com/techstorm/netcastdigital/compatibility/CompatibilityScaleGestureListener.java
@@ -0,0 +1,6 @@
+package com.techstorm.netcastdigital.compatibility;
+
+
+public interface CompatibilityScaleGestureListener {
+	public boolean onScale(CompatibilityScaleGestureDetector detector);
+}
