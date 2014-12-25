8031d5f183641cae94733fc429e9ff2dee0b0a74
vinh.thien0301 <vinh.thien0301@gmail.com>
2014-12-26 00:14:43 +0700

    add ios facebook connect

--- PrayerLine/platforms/ios/www/cordova_plugins.js
+++ PrayerLine/platforms/ios/www/cordova_plugins.js
@@ -1,4 +1,4 @@
- cordova.define('cordova/plugin_list', function(require, exports, module) {
+cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
         "file": "plugins/com.paypal.cordova.mobilesdk/www/cdv-plugin-paypal-mobile-sdk.js",
@@ -6,12 +6,20 @@ module.exports = [
         "clobbers": [
             "PayPalMobile"
         ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.facebookconnect/facebookConnectPlugin.js",
+        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.paypal.cordova.mobilesdk": "2.2.1"
+    "com.paypal.cordova.mobilesdk": "2.2.1",
+    "com.phonegap.plugins.facebookconnect": "0.11.0"
 }
 // BOTTOM OF METADATA
 });
